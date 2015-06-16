81c5ad703cb3a47f4c6b8beceb58346b1d26bb4c
Sam Nguyen <sam.nguyen@sendgrid.com>
2015-06-15 18:49:35 -0700

    Refactor

--- docktails.go
+++ docktails.go
@@ -14,6 +14,7 @@ var (
 	red   = "\x1b[0;31m"
 	green = "\x1b[0;32m"
 	blue  = "\x1b[0;34m"
+	bold  = "\x1b[1m"
 	reset = "\x1b[0m"
 )
 
@@ -27,9 +28,43 @@ func (p *prefixWriter) Write(b []byte) (int, error) {
 	return p.w.Write(b)
 }
 
+func startDockerLogs(dockerClient *docker.Client, containerID string) {
+	container, err := dockerClient.InspectContainer(containerID)
+	if err != nil {
+		log.Fatalf("unable to get container: %s", err)
+	}
+
+	if !container.State.Running {
+		return
+	}
+
+	if container.Config.Tty {
+		return
+	}
+
+	log.Printf("starting docker logs for %s %s",
+		container.ID[:12], container.Config.Image)
+
+	name := strings.Trim(container.Name, "/")
+	logOpts := docker.LogsOptions{
+		Container:    containerID,
+		Follow:       true,
+		Tail:         "0",
+		Stdout:       true,
+		Stderr:       true,
+		OutputStream: &prefixWriter{os.Stdout, green + name + reset + "  "},
+		ErrorStream:  &prefixWriter{os.Stderr, red + name + reset + "  "},
+	}
+
+	err = dockerClient.Logs(logOpts)
+	if err != nil {
+		log.Fatalf("unable to start docker logs: %s", err)
+	}
+}
+
 func main() {
 	log.SetFlags(0)
-	log.SetPrefix(blue + "dockertail" + reset + "  ")
+	log.SetPrefix(blue + "docktails" + reset + "  ")
 
 	dockerHost := os.Getenv("DOCKER_HOST")
 
@@ -45,62 +80,21 @@ func main() {
 
 	log.Printf("Connected to docker host: %s", dockerHost)
 
-	baselogOpts := docker.LogsOptions{
-		Follow: true,
-		Tail:   "0",
-		Stdout: true,
-		Stderr: true,
-	}
-
+	// Tail logs on all existing containers that are running
 	containers, err := dockerClient.ListContainers(docker.ListContainersOptions{All: true})
 	for _, c := range containers {
-		if strings.HasPrefix(c.Status, "Exited") {
-			continue
-		}
-
-		log.Printf("Starting docker logs for %s image=%s status=%s", c.ID[:12], c.Image, c.Status)
-		logOpts := baselogOpts
-		logOpts.Container = c.ID
-		stdoutWriter := prefixWriter{os.Stdout, green + c.Names[0] + reset + "  "}
-		stderrWriter := prefixWriter{os.Stderr, red + c.Names[0] + reset + "  "}
-		logOpts.OutputStream = &stdoutWriter
-		logOpts.ErrorStream = &stderrWriter
-
-		go func() {
-			err := dockerClient.Logs(logOpts)
-			if err != nil {
-				log.Fatalf("unable to start docker logs: %s", err)
-			}
-		}()
+		go startDockerLogs(dockerClient, c.ID)
 	}
 
+	// Listen for new containers to be started, and tail logs on those
 	eventChan := make(chan *docker.APIEvents)
-
 	dockerClient.AddEventListener(eventChan)
-
 	for event := range eventChan {
-		log.Printf("%s %s container=%s", event.From, event.Status, event.ID)
+		log.Printf("docker event %s%s%s %s container=%s", bold, reset, event.Status, event.From, event.ID[:12])
 
 		switch event.Status {
 		case "start":
-			logOpts := baselogOpts
-			logOpts.Container = event.ID
-
-			container, err := dockerClient.InspectContainer(event.ID)
-			if err != nil {
-				log.Fatalf("unable to get container: %s", err)
-			}
-
-			stdoutWriter := prefixWriter{os.Stdout, green + container.Name + reset + "  "}
-			stderrWriter := prefixWriter{os.Stderr, red + container.Name + reset + "  "}
-			logOpts.OutputStream = &stdoutWriter
-			logOpts.ErrorStream = &stderrWriter
-			go func() {
-				err := dockerClient.Logs(logOpts)
-				if err != nil {
-					log.Fatalf("unable to start docker logs: %s", err)
-				}
-			}()
+			go startDockerLogs(dockerClient, event.ID)
 		}
 	}
 }
